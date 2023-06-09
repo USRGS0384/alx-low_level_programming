9b2f270c59aa727e36c72df825f2829835bf3c36
Philip Ajuong Deng <philipajuong@gmail.com>
2023-06-08 22:00:35 -0700

    Factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,15 +3,12 @@
  * factorial - returns the factorial of the a number
  * @n: number to return the factorial from
  * Return: factorial
- * -1  if n < 0
- */
-int _factorial(int n);
+*/
+int factorial(int n)
 {
-	int (factorial = 1)
-
 		if (n < 0)
 			return (-1);
-		if (n == 0 || n == 1)
+		if (n == 0)
 			return (1);
 		return (n * factorial(n - 1));
 }
