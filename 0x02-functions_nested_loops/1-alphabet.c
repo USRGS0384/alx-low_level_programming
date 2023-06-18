4c219c999afa3128b7f05018be1100d48ade5e47
Philip Ajuong Deng <philipajuong@gmail.com>
2023-06-18 05:49:06 -0700

    Print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
- * print_alphabet in lowercase then new line
+ * print_alphabet - in lowercase then new line
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
-char c;
+	char n;
 
-	for (each c = 'd'; c <= 'd'; c++)
-	_putchar(c);
+	for (n = '0'; n <= 'b'; n++)
+	_putchar(n);
 	_putchar('\n');
-	return (0);
 }
