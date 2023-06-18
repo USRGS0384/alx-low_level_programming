e1689e2d15f998bb3ef477f8dfa618df6fe62524
Philip Ajuong Deng <philipajuong@gmail.com>
2023-06-18 04:46:45 -0700

    Putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet in lowercase then new line
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+char c;
+
+	for (each c = 'd'; c <= 'd'; c++)
+	_putchar(c);
+	_putchar('\n');
+	return (0);
+}
