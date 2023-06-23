654a31c091b711eac4778b9c27b4eff7cb8b6311
Philip Ajuong Deng <philipajuong@gmail.com>
2023-06-23 04:19:58 -0700

    Print last digit

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -7,12 +7,10 @@
  */
 int main(void)
 {
-    int r;
+	int r;
 
-    print_last_digit(98);
-    print_last_digit(0);
-    r = print_last_digit(-1024);
-    _putchar('0' + r);
-    _putchar('\n');
-    return (0);
+	r = print_last_digit(0);
+	_putchar('0' + r);
+	_putchar('\n');
+	return (0);
 }
