8aefaf65ba10a4fbbddc62f25dba39934c8a4529
Philip Ajuong Deng <philipajuong@gmail.com>
2023-06-23 09:06:13 -0700

    Natural counts

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - prints multiple counts of numbers of 3 or 5 below 1024
+* Return: return 0 (success)
+*/
+int main(void)
+{
+	int i, add = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+	if ((i % 3) == 0 && (i % 5) == 0)
+	add += i;
+	}
+	printf("%d\n", add);
+	return (0);
+}
