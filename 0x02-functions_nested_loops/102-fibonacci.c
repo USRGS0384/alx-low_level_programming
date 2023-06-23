41ad1759638af94444bd043474fb71a258203a76
Philip Ajuong Deng <philipajuong@gmail.com>
2023-06-23 09:19:56 -0700

    Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - displayout the first 50 numbers
+* fibonacci numbers of students
+* Return: return 0 (Success)
+*/
+int main(void)
+{
+	int x;
+	unsigned long y1 = 0, y2 = 1, y3;
+	
+	for (x = 0; x < 50; x++)
+	{
+	y3 = y1 + y2;
+	printf("%lu", y3);
+	y1 = y2;
+	y2 = y3;
+	if (x == 49)
+	printf("\n");
+	else
+	printf(", ");
+	}
+	return (0);
+}
