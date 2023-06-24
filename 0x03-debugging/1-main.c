ca74cd2e22376a728f771cb096016d8e70fc30c8
Philip Ajuong Deng <philipajuong@gmail.com>
2023-06-24 05:50:06 -0700

    1 Main c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - reasons of an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int a;
+
+	printf("Infinite loop incoming :(\n");
+
+	a = 0;
+	/*
+	*while (a < 10)
+	*{
+	*putchar(a);
+	*}
+	*/
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int largest_number(int a, int int b, int c);
+void print_remaining_days(int month, int day, int year);
+int covert_days(int month, int days);
 
 #endif /* MAIN_H */
