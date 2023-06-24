42f9b0b4f801d7660569d32bd640716a61b967f7
Philip Ajuong Deng <philipajuong@gmail.com>
2023-06-23 22:19:54 -0700

    O main c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - examine if integer would be  positive or negative
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive _or_ negative(i);
+	return (0);
+}
