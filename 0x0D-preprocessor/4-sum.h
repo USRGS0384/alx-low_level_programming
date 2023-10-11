8229f44cbd72aa98d845fbadf899098d947e265d
root <philipajuong@gmail.com>
2023-10-11 03:48:46 -0700

    0x0D task 5

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,15 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * main - print the value of the sum x and y
+ *
+ * @x: print x value
+ * @y: print y value
+ * Return: Always 0 (Success)
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
+
