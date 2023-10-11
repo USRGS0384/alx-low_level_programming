a617ec77ae7db1c9be54a13c3f3ac5d5bfe56a79
root <philipajuong@gmail.com>
2023-10-11 02:27:23 -0700

    0x0D task 4

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef MACROLIKE_H
+#define MACROLIKE_H
+
+/**
+ * MACRO_LIKE - absolute value of a number x
+ *
+ * @x: type in the value of macro
+ *
+ * Return: Header file with macrolike value.
+ */
+
+#define ABS(x) ((x) < 0 ? -(x) * (x) : (x))
+
+#endif
