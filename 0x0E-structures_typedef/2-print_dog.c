ee201921301f26a66a238a2ada74c33103fde663
root <philipajuong@gmail.com>
2023-10-13 02:32:04 -0700

    0x0E task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - a struct dog
+ * @d: print strcut dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name  == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
