a77859e15271064581ff1c60c180ee66b02897f0
root <philipajuong@gmail.com>
2023-10-13 02:49:49 -0700

    0x0E task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,8 +12,12 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == NULL)
+
 		d->name = "(nil)";
+
 	if (d->owner == NULL)
+
 		d->owner = "(nil)";
+
 	printf("name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
