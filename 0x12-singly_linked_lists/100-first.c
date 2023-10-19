a2ad1272468d47cb61b21cbfac4369504fc9895b
USRGS0384 <philipajuong@gmail.com>
2023-10-19 14:59:59 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * bmain - statements to be excecuted
+ *
+ * Return: the given instructions.
+ */
+
+void __attribute__ ((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
