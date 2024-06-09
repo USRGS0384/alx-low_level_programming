2016f26de0250eb712a1a526b6646d3c9f8f73e4
Philip Ajuong Deng <philipajuong@gmail.com>
2024-06-09 16:58:18 +0300

    Make files

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,39 +1,104 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
 
 /**
- * main - Entry point
- *@argc: number of arguments
- *@argv: string with arguments
- * Return: Always 0 (Success)
+ * main - function that multiplies two positive numbers.
+ * @argc: number of arguments.
+ * @argv: pointer to array of arguments.
+ *
+ * Print the result, followed by a new line.
+ * If the number of arguments is incorrect, print Error, followed by a,
+ * new line, and exit with a status of 98.
+ * num1 and num2 should only be composed of digits. If not, print Error,
+ * followed by a new line, and exit with a status of 98.
+ * You are allowed to use more than 5 functions in your file.
+ *
+ * Return: always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
-	long int multi;
-	int i, j;
+	char *str1, *str2;
+	int length1, length2, length, i, carry, digit1, digit2, *result, a = 0;
 
-	multi = 0;
-	if (argc != 3)
+	str1 = argv[1], str2 = argv[2];
+	if (argc != 3 || !is_digit(str1) || !is_digit(str2))
+		errors();
+	length1 = _strlen(str1);
+	length2 = _strlen(str2);
+	length = length1 + length2 + 1;
+	result = malloc(sizeof(int) * length);
+	if (!result)
+		return (1);
+	for (i = 0; i <= length1 + length2; i++)
+		result[i] = 0;
+	for (length1 = length1 - 1; length1 >= 0; length1--)
 	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		digit1 = str1[length1] - '0';
+		carry = 0;
+		for (length2 = _strlen(str2) - 1; length2 >= 0; length2--)
 		{
-			if (!(isdigit(argv[i][j])))
-			{
-				printf("Error\n");
-				exit(98);
-			}
+			digit2 = str2[length2] - '0';
+			carry += result[length1 + length2 + 1] + (digit1 * digit2);
+			result[length1 + length2 + 1] = carry % 10;
+			carry /= 10;
 		}
+		if (carry > 0)
+			result[length1 + length2 + 1] += carry;
 	}
-	multi = atoi(argv[1]) * atoi(argv[2]);
-	printf("%ld\n", multi);
+	for (i = 0; i < length - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
+}
+
+/**
+ * is_digit - function that checks if a string contains a non-digit char.
+ * @str: pointer to null-terminated byte string to be examined.
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise.
+ */
+int is_digit(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @str: pointer to null-terminated byte string to be examined.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * errors - function that handles errors for main.
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
 }
