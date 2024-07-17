e9a1d7051dee5d505fc7f5f218f511fd50ce3f8b
Philip Ajuong Deng <philipajuong@gmail.com>
2024-07-17 09:59:14 +0200

    0x1E-search_algorithms

--- 0x1E-search_algorithms/104-main.c
+++ 0x1E-search_algorithms/104-main.c
@@ -18,3 +18,4 @@ int main(void)
     printf("Found %d at index: %d\n\n", 5, advanced_binary(array, size, 5));
     printf("Found %d at index: %d\n", 999, advanced_binary(array, size, 999));
     return (EXIT_SUCCESS);
+}
